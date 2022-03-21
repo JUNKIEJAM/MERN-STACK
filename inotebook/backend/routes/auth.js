3b554d1e6b4f51478f02fb063036605b54952e68
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2022-03-21 12:23:58 +0530

    Creating Login Endpoint & sending auth token

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -54,11 +54,11 @@ router.post('/createuser',[
     
     //res.json({user})
     res.json({authToken});
-    
+
 }
 catch(error){
     console.log(error.message)
-    res.status(500).send("Some error occured")
+    res.status(500).send("Internal Server Occured")
 }
 
     //.then(user=>res.json(user)).catch(err=>{console.log(err)
@@ -66,5 +66,54 @@ catch(error){
 
 //    res.send(req.body);
 })
+
+// Authenticate a user  using POST: /api/uth/createuser // No login required
+
+router.post('/login',[
+    
+    body('email').isEmail(),
+    body('password','Password cannot be blank').exists(),
+   
+],async(req,res)=>{
+
+    // if there are errors return bad request and the errors
+    const errors = validationResult(req);
+  
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email,password}=req.body;
+    
+    try{
+let user=await user.findOne(email);
+
+if(!user){
+    return res.status(400).json({error:"Please login with correct Credentials"})
+}
+
+const passwordCompare=await bcrypt.compare(password,user.password);
+
+if(!passwordCompare){
+    return res.status(400).json({error:"Please login with correct Credentials"})
+}
+
+const data={
+    user:{
+     id:user.id   
+    }
+}
+
+const authToken= jwt.sign(data,JWT_SECRET);
+res.json(authToken);
+    
+}
+catch(error){
+    console.log(error.message)
+    res.status(500).send("Internal Server Occured")
+}
+
+})
+
 
 module.exports=router
